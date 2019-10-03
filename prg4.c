1fba00fc4c09327f2bbb4aa436ff1ecb55d1d7c9
dharshinis-it19 <dharshinis.it19@bitsathy.ac.in>
2019-10-03 10:08:28 +0530

    first commit

---
+++ prg4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main{
+int i,fact=1,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+ factorial=factorial*i;
+}
+printf("\n factorial 0f %d=%d",n,factorial);
+return 0;
+}
