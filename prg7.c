a81a077b4a28c922730eaa7e33a252f35a685e94
dharshinis-it19 <dharshinis.it19@bitsathy.ac.in>
2019-10-03 10:32:54 +0530

    first commit

---
+++ prg7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int X,Y,Z;
+printf("Enter the train length");
+scanf("%d",&X);
+printf("Enter the bridge length");
+scanf("%d",&Y);
+Z=X+Y;
+printf("%d",Z);
+return o;
+}
